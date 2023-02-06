80a61181cb3f242ea00fe08a7fff7723620cb87a
Brandon Zubia <76754843+10brazub@users.noreply.github.com>
2023-02-06 01:02:21 -0600

    Update QuoteControllerTest.java

--- summative-project/src/test/java/com/company/summativeproject/Controller/QuoteControllerTest.java
+++ summative-project/src/test/java/com/company/summativeproject/Controller/QuoteControllerTest.java
@@ -1,1 +1,32 @@
+package com.company.summativeproject.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(QuoteController.class)
+public class QuoteControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnOkStatus() throws Exception {
+
+        mockMvc.perform(get("/quote"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+
+}
